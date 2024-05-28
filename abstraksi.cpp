c60499e9f73e4437844343de9da905684c5cbc12
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-28 15:57:12 +0700

    commit14

---
+++ abstraksi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {
+private: string x, y;
+
+public:
+
+
+
+    void setXY(string a, string b) {
+        x = a;
+        x = b;
+    }
+
+    void display() {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
