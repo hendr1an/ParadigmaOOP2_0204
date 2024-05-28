403fbe6cb48f4e1e6bfcc7b694745c62bb586451
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-28 14:52:41 +0700

    commit2

--- solvemultiwaris.cpp
+++ solvemultiwaris.cpp
@@ -12,3 +12,18 @@ public:
         cout << "orang dibuat dengan umur " << umur << "\n" << endl;
     }
 };
+
+class pekerja : public orang {
+public:
+
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : public orang {
+public:
+
+};
